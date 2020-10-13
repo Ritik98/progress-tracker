f9b5c91b47cc75572b19c00b9ad59c60aa0fb88f
Ritik agarwal <ritik.9869@gmail.com>
2020-10-13 12:35:59 +0530

    Updates

---
+++ precache-manifest.a1d9a3afcf006d95c94fbe5c3d38eaf3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3afc973b37ef799926e5778d5d14748",
+    "url": "/progress-tracker/index.html"
+  },
+  {
+    "revision": "1b35abcc069d10d83bd6",
+    "url": "/progress-tracker/static/css/2.d1fc3ea7.chunk.css"
+  },
+  {
+    "revision": "b94adf31cad1650b875a",
+    "url": "/progress-tracker/static/css/main.3cb07df0.chunk.css"
+  },
+  {
+    "revision": "1b35abcc069d10d83bd6",
+    "url": "/progress-tracker/static/js/2.30f24d59.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/progress-tracker/static/js/2.30f24d59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b94adf31cad1650b875a",
+    "url": "/progress-tracker/static/js/main.60f22520.chunk.js"
+  },
+  {
+    "revision": "2ab18c1ba769843b63d0",
+    "url": "/progress-tracker/static/js/runtime-main.ff449d50.js"
+  }
+]);
